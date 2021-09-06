7fcf8dce4fda9983b8da592ba0e6752fe8390cad
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-06 09:31:13 +0100

    rest

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -25,4 +25,11 @@ String getControl()
 return	"l annotation Controller signifie Nous pouvons annoter les contrôleurs classique sous forme de composants";
 
 }
+@GetMapping("restControllers")
+String getRest()
+{
+return "@RestController est une version spécialisée du contrôleur. Il inclut\r\n"
+		+ "les annotations @Controller et @ResponseBody et, par conséquent, simplifie la\r\n"
+		+ "mise en œuvre du contrôleur";	
+}
 }
