6a74f7c483a7e99dbc5cc2271dfde3546abaeee7
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-06 09:23:48 +0100

    page1

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class HomeController {
+@GetMapping("")
+ModelAndView acceuil()
+{
+ModelAndView page=new ModelAndView("index.html");
+return page;
+}
+}
